09564cccb1e9565634ad6766001ddc43c99730bc
tobias <tobias.kamm@pm.me>
2021-02-05 13:38:03 +0100

    Simplified if-statement

--- app/src/main/java/de/tobiundmario/secrethitlermobilecompanion/RecyclerViewAdapters/EventCardRecyclerViewAdapter.java
+++ app/src/main/java/de/tobiundmario/secrethitlermobilecompanion/RecyclerViewAdapters/EventCardRecyclerViewAdapter.java
@@ -142,8 +142,9 @@ public class EventCardRecyclerViewAdapter extends RecyclerView.Adapter<DimmableV
     }
 
     private void processLongClick(GameEvent event, int position) {
+        boolean notLastLegislativeSession = (event instanceof LegislativeSession && ((LegislativeSession) event).getSessionNumber() != LegislativeSessionManager.legSessionNo - 1);
         if(GameEventsManager.editingEnabled) {
-            if (!GameManager.isManualMode() && (event instanceof LegislativeSession && ((LegislativeSession) event).getSessionNumber() != LegislativeSessionManager.legSessionNo - 1)) {
+            if (!GameManager.isManualMode() && notLastLegislativeSession) {
                 Toast.makeText(c, c.getString(R.string.toast_message_edit_blocked), Toast.LENGTH_LONG).show();
             } else {
                 event.isEditing = true;
